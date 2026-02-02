95fda02e27efdcea3b1bd423109bdbe7df083a85
bytem0use <webofmusic3@gmail.com>
2026-02-01 21:04:26 -0800

    Powers will now still be with player when player dies

--- src/main/java/net/bytem0use/origins/client/OriginsClient.java
+++ src/main/java/net/bytem0use/origins/client/OriginsClient.java
@@ -1,12 +1,25 @@
 package net.bytem0use.origins.client;
 
+import net.bytem0use.origins.api.PowerAPI;
 import net.bytem0use.origins.tester.OriginsKeybindings;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
 
 public class OriginsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
         OriginsKeybindings.register();
+
+        ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) -> {
+            if (!alive) {
+                for (StatusEffectInstance effect : oldPlayer.getStatusEffects()) {
+                    if (effect.getEffectType() instanceof PowerAPI) {
+                        newPlayer.addStatusEffect(new StatusEffectInstance(effect));
+                    }
+                }
+            }
+        });
     }
 }
